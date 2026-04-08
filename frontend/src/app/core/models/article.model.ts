6b92fc56d2d16c1a362665e879e5cac4aa301c76
Saeed Owais <saidewisali@gmail.com>
2026-04-08 16:31:05 +0200

    fixed reading time ui

--- frontend/src/app/core/models/article.model.ts
+++ frontend/src/app/core/models/article.model.ts
@@ -1,3 +1,5 @@
+import { Comment } from './comment.model';
+
 export interface Article {
   id: number;
   title: string;
@@ -5,7 +7,9 @@ export interface Article {
   content: string | null;
   imageUrl: string;
   category: string;
+  readTimeInMiniutes: number;
   tags: string[];
+  comments: Comment[] | null;
 }
 
 export interface AdminArticle {
@@ -17,6 +21,7 @@ export interface AdminArticle {
   updatedAt: string | null;
   imageUrl: string;
   views: number;
+  readTimeInMiniutes: number;
   isPublished: boolean;
   isDeleted: boolean;
 }
