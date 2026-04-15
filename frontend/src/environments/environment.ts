4ab445eb0fe525bd6127e8a50b9fd0ab9c0265d2
Saeed Owais <saidewisali@gmail.com>
2026-04-15 22:11:08 +0200

    replace tareekhalshoop with tareekhalshuob.com in environments

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://tareekhalshoob.com',
+  apiBaseUrl: 'https://tareekh-alshuob.com',
   i18nPrefix: './assets/i18n/',
   supabaseUrl: 'https://lnqrasqeqgwlumjuosye.supabase.co',
   supabaseKey: 'sb_publishable_7Cgp2piheikXJrrjwJnNpA__Xga2ECk'
