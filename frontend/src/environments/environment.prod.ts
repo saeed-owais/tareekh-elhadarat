34839632c4e7b618e357c22441b167ab929d8164
Saeed Owais <saidewisali@gmail.com>
2026-04-15 22:25:12 +0200

    fix apibaseUrl

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://tareekh-alshuob.com',
+  apiBaseUrl: 'https://tareekhalshuob.com',
   i18nPrefix: '/app/assets/i18n/',
   supabaseUrl: 'https://lnqrasqeqgwlumjuosye.supabase.co',
   supabaseKey: 'sb_publishable_7Cgp2piheikXJrrjwJnNpA__Xga2ECk'
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://tareekh-alshuob.com',
+  apiBaseUrl: 'https://tareekhalshuob.com',
   i18nPrefix: './assets/i18n/',
   supabaseUrl: 'https://lnqrasqeqgwlumjuosye.supabase.co',
   supabaseKey: 'sb_publishable_7Cgp2piheikXJrrjwJnNpA__Xga2ECk'
